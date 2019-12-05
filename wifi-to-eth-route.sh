3f302003b7f84d95ba2858ebac7e71e955504774
Tim <xyala@overclocking-tv.com>
2019-12-05 15:32:18 -0500

    Update wifi-to-eth-route.sh

--- wifi-to-eth-route.sh
+++ wifi-to-eth-route.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Share Wifi with Eth device
+# Share Eth (Pi Lan) with Eth USB device
 #
 #
 # This script is created to work with Raspbian Stretch
@@ -17,21 +17,23 @@ netmask="255.255.255.0"
 dhcp_range_start="192.168.2.2"
 dhcp_range_end="192.168.2.100"
 dhcp_time="12h"
-eth="eth1"
-wlan="eth0"
+#eth1 is the Pi USB lan connector
+ethUSB="eth1"
+#eth is the Pi eth port
+eth="eth0"
 
-iptables -t nat -A POSTROUTING -o $wlan -j MASQUERADE
-iptables -A FORWARD -i $wlan -o $eth -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i $eth -o $wlan -j ACCEPT
+iptables -t nat -A POSTROUTING -o $eth -j MASQUERADE
+iptables -A FORWARD -i $eth -o $ethUSB -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i $ethUSB -o $eth -j ACCEPT
 
 sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 
-ifconfig $eth $ip_address netmask $netmask
+ifconfig $ethUSB $ip_address netmask $netmask
 
 # Remove default route created by dhcpcd
-ip route del 0/0 dev $eth &> /dev/null
+ip route del 0/0 dev $ethUSB &> /dev/null
 
-echo -e "interface=$eth\n\
+echo -e "interface=$ethUSB\n\
 bind-interfaces\n\
 server=8.8.8.8\n\
 domain-needed\n\
